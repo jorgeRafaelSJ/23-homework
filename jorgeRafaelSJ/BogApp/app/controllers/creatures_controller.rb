2927c74940f567baf667558fc750aabcb146503a
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-11-07 17:35:41 -0800

    Added edit, update, and delete routes

--- jorgeRafaelSJ/BogApp/app/controllers/creatures_controller.rb
+++ jorgeRafaelSJ/BogApp/app/controllers/creatures_controller.rb
@@ -6,7 +6,8 @@ class CreaturesController < ApplicationController
   end
 
   def new
-
+  	@creature = Create.new
+  	render :new
   end
 
   def create
@@ -24,6 +25,30 @@ class CreaturesController < ApplicationController
   	id = params[:id]
   	@creature = Creature.find(id)
   	render :show
+  end
+
+  def edit
+  	id = params[:id]
+  	@creature = Creature.find(id)
+  	render :edit
+  end
+
+  def update
+  	creature_id = params[:id]
+  	creature = Creature.find(creature_id)
+
+  	updated_attributes = params.require(:creature).permit(:name, :description)
+  	creature.update_attributes(updated_attributes)
+
+  	redirect_to creature
+  end
+
+  def destroy
+  	id = params[:id]
+  	creature = Creature.find(id)
+  	creature.destroy
+
+  	redirect_to creatures_path
   end
 
 end
--- jorgeRafaelSJ/BogApp/config/routes.rb
+++ jorgeRafaelSJ/BogApp/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :creatures
 
-  root 'creatures#index', only: [:index, :new, :create, :show]
+  root 'creatures#index', only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
 end
